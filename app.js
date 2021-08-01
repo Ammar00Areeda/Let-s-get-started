4d57c4045d22c81d5eec902e147973155e828ee8
Ammar00Areeda <akiareeda@yahoo.com>
2021-08-01 16:49:21 +0300

    add

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+let userName2 = prompt('please enter your name');
+
+
+let userLastName = prompt('please enter your last name');
+
+
+let age = prompt('please enter your age');
+document.write(`<h2>${userName2 + ' ' + userLastName +  ' **and your age is ' + age}</h2>`);
+
+let favMeal = prompt('please enter your fav meal');
+document.write(`<h2>${favMeal + '  eeeee3333 i dont like it  ' }</h2>`);
